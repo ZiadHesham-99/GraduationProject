b06d7d7fae737d213274ef3d8113f41ae3472ba8
Tarek Alaa <103005836+Tarek191020@users.noreply.github.com>
2023-06-22 18:31:56 +0300

    pub Golden branch
    al golden wasal

--- Embededd/src/APP/DataAcquisition.c
+++ Embededd/src/APP/DataAcquisition.c
@@ -15,7 +15,7 @@
 #include "APP/DataAcquisition.h"
 
 static tstrRaspberryPiMsg LOC_strMsg;
-/*ana tareeeeeeeeeeeeeeeek */
+/*ana tareeeeeeek */
 void DAQ_vidInit(void)
 {
     LOC_strMsg.s8GyroX = 0;
